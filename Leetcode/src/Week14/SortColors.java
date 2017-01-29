4a2d14aee103097a00e74e4a12e9914c954b09c5
jlu86 <jlu.test.86@gmail.com>
2017-01-29 15:34:02 -0800

    Optimize Sort Colors

--- Leetcode/src/Week14/SortColors.java
+++ Leetcode/src/Week14/SortColors.java
@@ -9,32 +9,28 @@ public class Solution {
         int current = 0;
         int temp;
         while (current <= currentRight) {
-            if (nums[current] == 0) {
+            // Update leftmost
+            while (currentLeft < nums.length && nums[currentLeft] == 0) {
+                currentLeft++;
+            }
+            
+            // Update rightmost
+            while (currentRight >= 0 && nums[currentRight] == 2) {
+                currentRight--;
+            }
+            
+            if (nums[current] == 0 && currentLeft < current) {
                 // swap with the leftmost one
-                while (currentLeft < nums.length && nums[currentLeft] == 0) {
-                    currentLeft++;
-                }
-                if (currentLeft < current) {
-                    temp = nums[currentLeft];
-                    nums[currentLeft] = nums[current];
-                    nums[current] = temp;
-                    currentLeft++;
-                } else {
-                    current++;
-                }
-            } else if (nums[current] == 2) {
+                temp = nums[currentLeft];
+                nums[currentLeft] = nums[current];
+                nums[current] = temp;
+                currentLeft++;
+            } else if (nums[current] == 2 && currentRight > current) {
                 // swap with the rightmost one
-                while (currentRight >= 0 && nums[currentRight] == 2) {
-                    currentRight--;
-                }
-                if (currentRight > current) {
-                    temp = nums[currentRight];
-                    nums[currentRight] = nums[current];
-                    nums[current] = temp;
-                    currentRight--;
-                } else {
-                    current++;
-                }
+                temp = nums[currentRight];
+                nums[currentRight] = nums[current];
+                nums[current] = temp;
+                currentRight--;
             } else {
                 current++;
             }
