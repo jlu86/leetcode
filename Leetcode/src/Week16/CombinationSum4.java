1f829f66240df19f4c6c4289fc5346f996fe8153
jlu86 <jlu.test.86@gmail.com>
2017-02-12 11:38:49 -0800

    377. Combination Sum IV.v3.time exceed fixed.2.12

--- Leetcode/src/Week16/CombinationSum4.java
+++ Leetcode/src/Week16/CombinationSum4.java
@@ -1,4 +1,7 @@
 public class Solution {
+    // target -> num of combinations
+    HashMap<Integer, Integer> intermidiateResult = new HashMap<>();
+    
     public int combinationSum4(int[] nums, int target) {
         if (nums.length == 0) {
             return 0;
@@ -29,13 +32,24 @@ public class Solution {
         }
         
         int total = 0;
-        for (int i = 0; i < nums.length && nums[i] <= target; i++) {
-            if (target - nums[i] == 0) {
+        for (int i = nums.length-1; i >= 0; i--) {
+            if (nums[i] > target) {
+                continue;
+            }
+            
+            int nextTarget = target - nums[i];
+            if (nextTarget == 0) {
                 total++;
+            } else if (intermidiateResult.containsKey(nextTarget)) {
+                // already computed the result for nextTarget, use it directly
+                total += intermidiateResult.get(nextTarget);
             } else {
-                total += combinationSum4(nums, target - nums[i]);
+                total += combinationSum4(nums, nextTarget);
             }
         }
+        
+        // cache the intermidiate result
+        intermidiateResult.put(target, total);
         
         return total;
     }
