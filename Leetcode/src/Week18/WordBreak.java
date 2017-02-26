31f98badafd6ced03f4f5611de3e3d0410e48b67
jlu86 <jlu.test.86@gmail.com>
2017-02-25 16:07:03 -0800

    139. Word Break.v2.optimized.recursion.2/25

--- Leetcode/src/Week18/WordBreak.java
+++ Leetcode/src/Week18/WordBreak.java
@@ -1,46 +1,43 @@
 public class Solution {
+    // substring (i-end), can break to words or not
+    HashMap<Integer, Boolean> breakResult = new HashMap<>();
+    int maxWordLength = 0;
+    
     public boolean wordBreak(String s, List<String> wordDict) {
+        // Hashset of the dictionary
         HashSet<String> dict = new HashSet<>();
+        
         for (int i = 0; i < wordDict.size(); i++) {
-            dict.add(wordDict.get(i));
+            String word = wordDict.get(i);
+            if (word.length() > maxWordLength) {
+                maxWordLength = word.length;
+            }
+            dict.add(word);
         }
         
-        // record all the words met so far
-        Stack<String> existingWords = new Stack<>();
-        // the start index of current word
-        int lastStartIdx = 0;
-        // track current word
+        return wordBreak(s, 0, dict);  
+    }
+    
+    // return whether the substring (startIdx, s.length()-1) can break to words
+    public boolean wordBreak(String s, int startIdx, HashSet<String> dict) {
+        boolean canBreak = false;
         StringBuilder sb = new StringBuilder();
-        // current index
-        int i = 0;
-        while (true) {
-            for (; i < s.length(); i++) {
-                sb.append(s.charAt(i));
-                String word = sb.toString();
-                if (dict.contains(word)) {
-                    // a word is met
-                    existingWords.push(word);
-                    // reset all the variables
-                    lastStartIdx = i+1;
-                    sb = new StringBuilder();
+        for (int length = 0; length < maxWordLength && startIdx + length < s.length(); length++) {
+            int curIdx = startIdx + length;
+            sb.append(s.charAt(curIdx));
+            if (dict.contains(sb.toString()) {
+                // compute the result starting from curIdx+1
+                if (curIdx == s.length()+1) {
+                    canBreak = true;
+                } else if (breakResult.containsKey(curIdx+1)) {
+                    canBreak = breakResult.get(curIdx+1);
+                } else {
+                    canBreak = wordBreak(s, curIdx+1, dict);
                 }
             }
-            
-            if (lastStartIdx == s.length()) {
-                // the last set is also a word, return directly
-                return true;
-            }
-            
-            // the last set of characters is not a word, backtrace and try again
-            if (existingWords.empty()) {
-                // can not back anymore
-                return false;
-            }
-            
-            // back to the last word and try again
-            sb = new StringBuilder(existingWords.pop());
-            i = lastStartIdx;
-            lastStartIdx = i - sb.length();
         }
+        breakResult.put(startIdx, canBreak);
+        
+        return canBreak;
     }
 }
