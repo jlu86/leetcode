a33b67ae9e9630ddda0385879bfed27417191b42
jlu86 <jlu.test.86@gmail.com>
2017-03-05 14:59:30 -0800

    261. Graph Valid Tree.v2.optimized.3/5

--- Leetcode/src/Week19/GraphValidTree.java
+++ Leetcode/src/Week19/GraphValidTree.java
@@ -1,59 +1,58 @@
 public class Solution {
     public boolean validTree(int n, int[][] edges) {
-        if (n <= 1) {
-            return true;
+        if (edges.length == 0) {
+            // Tricky input, why n=1 and edges=[] returns true, but n=2 and edges=[] returns false???
+            return n == 1 ? true : false;
         }
         
-        // Build a hashmap from parent to list of child nodes
-        HashMap<Integer, List<Integer>> edgeMap = new HashMap<>();
-        // Use a hashset to note whether there are parent nodes for current nodes
-        HashSet<Integer> nonRootSet = new HashSet<>();
+        // Build a hashmap from parent to list of child nodes (undirected, so both parent and child nodes could be the key)
+        HashMap<Integer, HashSet<Integer>> edgeMap = new HashMap<>();
         for (int i = 0; i < edges.length; i++) {
-            List<Integer> edgeList = new ArrayList<>();
+            HashSet<Integer> edgeList = new HashSet<>();
             int parent = edges[i][0];
             int child = edges[i][1];
+            // Save the mapping from a to b
             if (edgeMap.containsKey(parent)) {
                 edgeList = edgeMap.get(parent);
             }
             edgeList.add(child);
             edgeMap.put(parent, edgeList);
-            // Update non-root set
-            nonRootSet.add(child);
-        }
-       
-       // Find root node 
-        int root = -1;
-        for (int i = 0; i < n; i++) {
-            if (!nonRootSet.contains(i)) {
-                root = i;
+            
+            // Save the mapping from b to a
+            edgeList = new HashSet<>();
+            if (edgeMap.containsKey(child)) {
+                edgeList = edgeMap.get(child);
             }
-        }
-        
-        if (root == -1) {
-            return false;
+            edgeList.add(parent);
+            edgeMap.put(child, edgeList);
         }
         
         // Create a stack to store the nodes in the current path
         Stack<Integer> stack = new Stack<>();
         // Create a hashset to record whether the node has been visited before as a child node
         HashSet<Integer> visited = new HashSet<>();
-        stack.push(0);
-        visited.add(0);
+        
+        int start = edges[0][0];
+        stack.push(start);
+        visited.add(start);
         while (!stack.empty()) {
             int parent = stack.peek();
-            List<Integer> children = edgeMap.get(parent);
+            HashSet<Integer> children = edgeMap.get(parent);
             if (children == null || children.isEmpty()) {
                 stack.pop();
             } else {
                 // Get the last child node
-                int child = children.get(children.size() - 1);
-                children.remove(children.size() - 1);
+                int child = children.iterator().next();
                 if (visited.contains(child)) {
                     return false;
                 }
                 
                 stack.push(child);
                 visited.add(child);
+                
+                // Remove the visited node from edge map
+                children.remove(child);
+                edgeMap.get(child).remove(parent);
             }
         }
         
